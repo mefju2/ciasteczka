56905f818cfbd97af5d6f9011fd218caa51a7ceb
mefju2 <55331075+mefju2@users.noreply.github.com>
2020-03-23 01:05:28 +0100

    Add files via upload

---
+++ ciasteczka/backToTop.js
@@ -0,0 +1,11 @@
+mybutton = document.getElementById("back");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }   
+}
